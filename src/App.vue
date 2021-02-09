d44ff42c00f5c24bfeed4aa6aaae234d083fd0df
남상준 <namjals615@gmail.com>
2021-02-09 14:27:10 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <v-blockly id="blockly"></v-blockly>
+  </div>
+</template>
+
+<script>
+import VBlockly from "v-blockly";
+import "v-blockly/dist/VBlockly.css";
+
+export default {
+  name: "App",
+  components: {
+    "v-blockly": VBlockly,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#blockly {
+  height: 80vh;
+}
+</style>
